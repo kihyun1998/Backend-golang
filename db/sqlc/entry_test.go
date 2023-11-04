11c5d88d90377077afe9ef00992fa59590bd0483
wantSome <kihyun1998@naver.com>
2023-11-04 15:17:33 +0900

    Create entry_test.go

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,91 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"simplebank/util"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func CreateRandomEntry(t *testing.T) Entry {
+	account1 := CreateRandomAccount(t)
+	arg := CreateEntryParams{
+		AccountsID: account1.ID,
+		Amount:     util.RandomAmount(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountsID, entry.AccountsID)
+	require.Equal(t, arg.Amount, entry.Amount)
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	CreateRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := CreateRandomEntry(t)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountsID, entry2.AccountsID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestUpdateEntry(t *testing.T) {
+	entry1 := CreateRandomEntry(t)
+	arg := UpdateEntryParams{
+		ID:     entry1.ID,
+		Amount: util.RandomAmount(),
+	}
+	entry2, err := testQueries.UpdateEntry(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, arg.ID, entry2.ID)
+	require.Equal(t, entry1.AccountsID, entry2.AccountsID)
+	require.Equal(t, arg.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestDeleteEntry(t *testing.T) {
+	entry1 := CreateRandomEntry(t)
+	err := testQueries.DeleteEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, entry2)
+}
+
+func TestListEntries(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		CreateRandomEntry(t)
+	}
+	arg := ListEntriesParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
