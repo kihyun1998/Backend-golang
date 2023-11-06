b382ba016054205159e4f272320128118d7bc92f
wantSome <kihyun1998@naver.com>
2023-11-06 13:36:19 +0900

    get testdb

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -15,17 +15,18 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-
+	var err error
 	// db 연결 테스트
-	conn, err := sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect : ", err)
 	}
 
 	// db 접속 객체를 testQueries에 저장
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	// 테스트 시작
 	os.Exit(m.Run())
