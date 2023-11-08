fe319cdd5309635f1352e5eb418ddb4e6c065da7
wantSome <kihyun1998@naver.com>
2023-11-08 10:43:02 +0900

    use viper for config var

--- main.go
+++ main.go
@@ -5,21 +5,22 @@ import (
 	"log"
 	"simplebank/api"
 	db "simplebank/db/sqlc"
+	"simplebank/util"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	const (
-		dbDriver      = "postgres"
-		dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-		serverAddress = "0.0.0.0:8080"
-	)
+
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("CANNOT LOAD CONFIG", err)
+	}
 
 	// 서버 생성하려면 db에 연결하고 store 생성해야한다.
 
 	// db 연결
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect : ", err)
 	}
@@ -27,7 +28,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatalln("CANNOT START SERVER")
 	}
