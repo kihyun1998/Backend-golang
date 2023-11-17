0a1396683c65139dd651cbe5fe66e87d999514d0
wantSome <kihyun1998@naver.com>
2023-11-17 16:38:56 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,7 @@ echo "**run db migration**"
 source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
+chmod +x /app/start.h
+
 echo "***start the app***"
 exec "$@"
